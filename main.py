bed43c580d9e68a2bb1b4e3a3f8d7169975d3038
Ankit Kumar <ankit851999@users.noreply.github.com>
2023-10-09 14:16:52 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# from turtle import Turtle,Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("blue")
+# timmy.forward(200)
+# my_screen = Screen()
+# print(my_screen.canvwidth)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("adfad",[24,25,35,35,35,])
+table.add_column("adfad",[24,25,35,35,35,])
+table.align = "r"
+print(table)
